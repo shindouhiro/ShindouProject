e68690d6a4269107de5a058202702678a3d72f46
Hikaru <shindouhiro@gmail.com>
2024-06-21 14:52:03 +0800

    feat: 去掉根目录

--- admin/src/auth/auth.controller.ts
+++ admin/src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('用户鉴权')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
+  @ApiOperation({
+    summary: '鉴权', 
+  })
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async signIn(@Body() signInDto: Record<string, any>) {
