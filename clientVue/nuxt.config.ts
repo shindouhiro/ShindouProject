9ae6d46ee26cd26680839b18fc10f727e0461a1a
Hikaru <shindouhiro@gmail.com>
2024-06-25 14:26:13 +0800

    feat: 引入unocss

--- clientVue/nuxt.config.ts
+++ clientVue/nuxt.config.ts
@@ -1,7 +1,12 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@element-plus/nuxt'],
+  modules: [
+    '@element-plus/nuxt',
+    '@unocss/nuxt',
+  ],
   plugins: [
     '~/plugins/axios.ts',
     '~/plugins/mock.ts'
--- clientVue/pages/index.vue
+++ clientVue/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="mt-20">
         <users />
     </div>
 </template>
